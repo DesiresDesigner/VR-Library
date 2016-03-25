def68efd5302b515982083f11c9a3c9a7f652367
desiresdesigner <mialer489753>
2016-03-25 22:29:23 +0300

    Initial commit

---
+++ src/main/java/com/hackday/vrlibrary/HelloController.java
@@ -0,0 +1,19 @@
+package com.hackday.vrlibrary;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by DesiresDesigner on 3/25/16.
+ */
+
+@RestController
+//@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
