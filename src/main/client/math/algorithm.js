81d43bf74a27e24ddef0f4bbbb4af8d775b8c160
Anton <anton.veselyev@gmail.com>
2016-03-26 15:46:47 +0300

    added frontend

---
+++ src/main/client/math/algorithm.js
@@ -0,0 +1,7 @@
+/**
+ * Created by vesel on 2016-03-26.
+ */
+
+function embedGraph(graphJSON) {
+    const graph = JSON.parse(graphJSON);
+}
---
+++ src/main/client/math/point.js
@@ -0,0 +1,17 @@
+/**
+ * Created by vesel on 2016-03-26.
+ */
+
+class Point {
+    /**
+     * Create a point.
+     * @param {number} x - The x value.
+     * @param {number} y - The y value.
+     * @param {number} z - The z value.
+     */
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
