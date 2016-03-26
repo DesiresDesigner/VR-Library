5bd23fceaf07e18f1b6db41c1b6f9ff9ca8fc9ec
desiresdesigner <mialer489753>
2016-03-26 12:40:59 +0300

    html works!

---
+++ src/main/java/com/hackday/vrlibrary/config/MvcConfig.java
@@ -0,0 +1,12 @@
+package com.hackday.vrlibrary.config;
+
+/**
+ * Created by DesiresDesigner on 3/26/16.
+ */
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+}
